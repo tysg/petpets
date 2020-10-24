5b0a6ee9cbd8eac6f58b983e6d04ac438e7bbf4a
branson <ngbranson567@gmail.com>
2020-10-24 14:44:07 +0800

    rename routes

--- routes/pets.ts
+++ routes/pets.ts
@@ -3,11 +3,11 @@ import * as petsController from "../controllers/petsController";
 
 export const router = Router();
 
-router.get("/:owner", petsController.index);
-router.get("/:name/:owner", petsController.get);
-router.delete("/:name/:owner", petsController.remove);
 router.post("/", petsController.create);
-router.patch("/:name/:owner", petsController.update);
+router.patch("/:owner/:name", petsController.update);
+router.delete("/:owner/:name", petsController.remove);
+router.get("/:owner", petsController.index);
+router.get("/:owner/:name", petsController.get);
 
 
 export default router;
