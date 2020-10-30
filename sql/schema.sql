2cfc54e85d66ad908d18f3054436e4764c56af81
branson <ngbranson567@gmail.com>
2020-10-30 11:34:36 +0800

    ct and schedules schema and seed

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,6 +1,12 @@
 DROP TABLE IF EXISTS credit_card;
+DROP TABLE IF EXISTS specialised_for;
 DROP TABLE IF EXISTS pet;
 DROP TABLE IF EXISTS pet_category;
+DROP TABLE IF EXISTS pt_free_schedule;
+DROP TABLE IF EXISTS ft_leave_schedule;
+DROP TABLE IF EXISTS full_time_ct;
+DROP TABLE IF EXISTS part_time_ct;
+DROP TABLE IF EXISTS care_taker;
 DROP TABLE IF EXISTS person;
 CREATE TABLE person(
 	email varchar(64) PRIMARY KEY,
@@ -8,7 +14,8 @@ CREATE TABLE person(
 	password varchar(64) NOT NULL,
 	address varchar(64) NOT NULL,
 	phone int NOT NULL,
-	avatar_link varchar
+	avatar_link varchar,
+	ct_status smallint DEFAULT 0 CHECK (ct_status IN (0,1,2)) 
 );
 CREATE TABLE pet_category(
 	type_name varchar(64) PRIMARY KEY,
@@ -28,4 +35,35 @@ CREATE TABLE credit_card(
 	expiryDate Date,
 	securityCode smallint,
 	CONSTRAINT credit_card_id PRIMARY KEY (cardNumber, cardholder)
+);
+
+CREATE TABLE care_taker (
+	email varchar(64) PRIMARY KEY REFERENCES person(email) ON DELETE CASCADE,
+	rating decimal DEFAULT NULL --TODO look into how to implement
+);
+
+CREATE TABLE specialised_for (
+	email varchar(64) REFERENCES care_taker(email) ON DELETE CASCADE,
+	type_name varchar(64) REFERENCES pet_category(type_name) ON DELETE CASCADE,
+	CONSTRAINT specialied_for_id PRIMARY KEY (email, type_name)
+);
+
+CREATE TABLE part_time_ct (
+	email varchar(64) PRIMARY KEY REFERENCES care_taker(email) ON DELETE CASCADE
+);
+
+CREATE TABLE full_time_ct (
+	email varchar(64) PRIMARY KEY REFERENCES care_taker(email) ON DELETE CASCADE
+);
+
+CREATE TABLE pt_free_schedule (
+	email varchar(64) REFERENCES part_time_ct(email) ON DELETE CASCADE,
+	start_date date NOT NULL,
+	end_date date NOT NULL
+);
+
+CREATE TABLE ft_leave_schedule (
+	email varchar(64) REFERENCES full_time_ct(email) ON DELETE CASCADE,
+	start_date date NOT NULL,
+	end_date date NOT NULL
 );
