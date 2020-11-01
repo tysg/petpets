5bb4b3030790d46199154da5ea11358270454f9d
Tianyi <42670338+tysng@users.noreply.github.com>
2020-11-01 23:59:50 +0800

    Add seeds for pet

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -3,6 +3,15 @@ INSERT INTO pet_category VALUES ('cat', 5);
 INSERT INTO person VALUES ('jan@gmail.com', 'jan', '$2b$10$yHkpPyW2w/xqIeO4Efi2teHkkG4ZK8p64MWo0M6jJfcjwkNfBjcte', '123 Clementi Rd', '82792172', 'user', 'gravatar.com');
 INSERT INTO person VALUES ('p@gmail.com', 'p', '$2b$10$7Fl/H4DxhlQrfHeHAvrQyOXkHDBO5Cmwy9I4lKokJKjHyLORxujcy', '81 pgp Rd', '62353535', 'user');
 INSERT INTO person VALUES ( 'admin@email.com', 'Bran Bong', '$2b$10$JUPcABQnf.k8qqW44CHAnuNK4ieRo6YOlZXw/db8ecl6PM/Q3aNEO', 'php', '91231233', 'admin'); 
+
+-- pets
+INSERT INTO pet VALUES ('Apple','jan@gmail.com','cat','allergic to peanuts','a petty pet');
+INSERT INTO pet VALUES ('Pear','jan@gmail.com','dog','allergic to peanuts','a petty pet');
+INSERT INTO pet VALUES ('Orange','jan@gmail.com','cat','allergic to peanuts','a petty pet');
+INSERT INTO pet VALUES ('Crab','jan@gmail.com','dog','allergic to peanuts','a petty pet');
+INSERT INTO pet VALUES ('Pear','p@gmail.com','cat','allergic to peanuts','a petty pet');
+INSERT INTO pet VALUES ('Apple','p@gmail.com','cat','allergic to peanuts','a petty pet');
+
 -- care takers
 
 -- FULL TIME
