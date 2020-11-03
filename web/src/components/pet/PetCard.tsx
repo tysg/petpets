c6617de1e063edca1f3b8af02e9a2ee065c5d078
ChenJiehan318 <e0323526@u.nus.edu>
2020-11-03 20:54:29 +0800

    add profile page for pets

--- web/src/components/pet/PetCard.tsx
+++ web/src/components/pet/PetCard.tsx
@@ -1,7 +1,16 @@
-import React from "react";
-import { Avatar, Space, Card, Rate } from "antd";
+//import React from "react";
+import { Avatar, Space, Card, Rate, Descriptions } from "antd";
 import { CareTakerDetails } from "../../../../models/careTaker";
 import { Pet } from "../../../../models/pet";
+import {
+    Link,
+    Redirect,
+    Route,
+    RouteComponentProps,
+    Switch,
+    useRouteMatch
+} from "react-router-dom";
+import React, { PropsWithChildren } from "react";
 
 const { Meta } = Card;
 
@@ -19,12 +28,34 @@ const NameAndRating = (props: CareTakerDetails) => {
     );
 };
 
-const PetCard = (props: Pet) => {
+function ProfilePage(props: Pet) {
     return (
-        <Card hoverable onClick={console.log}>
-            <Meta
-                avatar={
-                    <Avatar
+        <Descriptions title="Pet Info" bordered>
+            <Descriptions.Item label="Pet Name">{props.name}</Descriptions.Item>
+            <Descriptions.Item label="Pet Category">{props.category}</Descriptions.Item>
+            <Descriptions.Item label="Pet Description">{props.description}</Descriptions.Item>
+            <Descriptions.Item label="Pet Requirements">{props.requirements}</Descriptions.Item>
+        </Descriptions>
+    );
+}
+
+const PetCard = (props: Pet) => {
+    const { path } = useRouteMatch();
+    return (
+        <><Switch>
+            <Route path={`/dashboard/owner/pets/${props.name}`} render={() => (
+            <ProfilePage
+              name={props.name}
+              owner={props.owner}
+              category={props.category}
+              description={props.description}
+              requirements={props.requirements}
+            />
+          )} />
+        </Switch>
+            <Card>
+                <Meta
+                    avatar={<Avatar
                         style={{
                             backgroundColor: "#f56a00",
                             verticalAlign: "middle"
@@ -32,11 +63,11 @@ const PetCard = (props: Pet) => {
                         size="large"
                     >
                         {props.name.charAt(0).toUpperCase()}
-                    </Avatar>
-                }
-                description={props.description}
-            />
-        </Card>
+                    </Avatar>}
+                    title={props.name}
+                    description={props.description} />
+                <p><Link to={`/dashboard/owner/pets/${props.name}`}>view profile</Link></p>
+            </Card></>
     );
 };
 export default PetCard;
