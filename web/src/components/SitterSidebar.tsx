4d736faba6c4da9b708afe86c68ae88287afd03a
ChenJiehan318 <e0323526@u.nus.edu>
2020-11-03 16:13:24 +0800

    change sitter sidebar

--- web/src/components/SitterSidebar.tsx
+++ web/src/components/SitterSidebar.tsx
@@ -16,9 +16,7 @@ const SitterSidebar = (props: RouteComponentProps) => {
         >
             <SubMenu key="sitter" icon={<LaptopOutlined />} title="Pet Sitter">
                 <Menu.Item key="pastjobs">Past Jobs</Menu.Item>
-                <Menu.Item key="sitter-2">option6</Menu.Item>
-                <Menu.Item key="sitter-3">option7</Menu.Item>
-                <Menu.Item key="sitter-4">option8</Menu.Item>
+                <Menu.Item key="sitter-2">Availability</Menu.Item>
             </SubMenu>
         </ResponsiveSidebar>
     );
