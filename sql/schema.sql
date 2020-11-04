91c029f5d76930f701d6f8e36b3c4390269223b6
Tianyi <42670338+tysng@users.noreply.github.com>
2020-11-04 15:47:06 +0800

    Fix db schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS credit_card CASCADE;
+DROP TABLE IF EXISTS specializes_in CASCADE; -- backwards compatible
 DROP VIEW IF EXISTS specializes_in CASCADE;
 DROP TABLE IF EXISTS pt_specializes_in CASCADE;
 DROP TABLE IF EXISTS ft_specializes_in CASCADE;
