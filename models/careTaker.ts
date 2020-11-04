8d6dbff9228eebfaa8f801293d905a59884501a7
Tianyi <42670338+tysng@users.noreply.github.com>
2020-11-05 02:44:38 +0800

    Error free caretaker get

--- models/careTaker.ts
+++ models/careTaker.ts
@@ -1,4 +1,5 @@
 import { ApiResponse } from "./index";
+import * as yup from "yup";
 
 export enum CaretakerStatus {
     notCt = 0,
@@ -6,10 +7,12 @@ export enum CaretakerStatus {
     fullTimeCt = 2
 }
 
-export interface SpecializesIn {
-    typeName: string;
-    ctPriceDaily: number;
-}
+export const SpecializesInSchema: yup.ObjectSchema<SpecializesIn> = yup
+    .object({
+        typeName: yup.string().defined(),
+        ctPriceDaily: yup.number().defined()
+    })
+    .defined();
 
 /**
  * PATCH api/caretakers/
@@ -21,6 +24,13 @@ export interface CareTaker {
     email: string;
     allSpecializes: SpecializesIn[];
 }
+
+export const CareTakerSchema: yup.ObjectSchema<CareTaker> = yup
+    .object({
+        email: yup.string().defined(),
+        allSpecializes: yup.array(SpecializesInSchema).defined()
+    })
+    .defined();
 
 /**
  * GET api/caretakers/
@@ -35,6 +45,18 @@ export interface CareTakerDetails {
     rating: number;
 }
 
+export const CareTakerDetailsSchema: yup.ObjectSchema<CareTakerDetails> = yup
+    .object({
+        fullname: yup.string().defined(),
+        phone: yup.number().defined(),
+        address: yup.string().defined(),
+        email: yup.string().defined(),
+        avatarUrl: yup.string().optional(),
+        caretakerStatus: yup.number().defined(),
+        rating: yup.number().defined()
+    })
+    .defined();
+
 /**
  * GET api/caretakers/:email
  */
@@ -42,10 +64,17 @@ export interface CareTakerSpecializesDetails extends CareTakerDetails {
     allSpecializes: SpecializesIn[];
 }
 
-export interface SpecializesDetails extends CareTakerDetails {
+export interface SpecializesIn {
     typeName: string;
     ctPriceDaily: number;
 }
+
+export const CareTakerSpecializesDetailsSchema: yup.ObjectSchema<CareTakerSpecializesDetails> = yup
+    .object({
+        allSpecializes: yup.array(SpecializesInSchema).defined()
+    })
+    .concat(CareTakerDetailsSchema)
+    .defined();
 
 /**
  * GET api/caretakers/
@@ -56,7 +85,7 @@ export type IndexResponse = ApiResponse<CareTakerDetails[], string>;
  * GET api/caretakers/search?start_date=2020-11-06&end_date=2020-11-08&pet_category=dog
  */
 export type SpecializesIndexResponse = ApiResponse<
-    SpecializesDetails[],
+    CareTakerSpecializesDetails[],
     string
 >;
 
