1f14c20eb3edb8067156818b49f0eda3c36da34e
Branson Ng <ngbranson567@gmail.com>
2020-11-07 00:23:44 +0800

    fixed bug lmao

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -30,7 +30,7 @@ INSERT INTO pt_specializes_in VALUES ('ptct2@gmail.com', 'Dog', 4);
 
 -- schedules
 INSERT INTO pt_free_schedule VALUES ('ptct@gmail.com', '2020-11-05', '2020-11-10');
-INSERT INTO ft_leave_schedule VALUES ('ftct@gmail.com', '2020-12-30', '2021-01-05');
+-- INSERT INTO ft_leave_schedule VALUES ('ftct@gmail.com', '2020-12-30', '2021-01-05');
 -- will throw not enought consecutive working days error
 -- INSERT INTO ft_leave_schedule VALUES ('ftct@gmail.com', '2020-09-20', '2021-09-30'); 
 
--- sql/triggers.sql
+++ sql/triggers.sql
@@ -306,7 +306,10 @@ BEGIN
 	) as cc
 	WHERE Date(cc.sd2)-Date(cc.ed1) >= 300;
 
-	IF new_start_year = new_end_year THEN
+	insert into count_limit VALUES (count_150);
+	insert into count_limit VALUES (count_300);
+
+	IF new_start_year != new_end_year THEN
 		IF count_150 = 4 OR (count_150 > 1 AND count_300 > 0) THEN 
 			RETURN NEW;
 		ELSE
@@ -325,129 +328,3 @@ $t$ LANGUAGE PLpgSQL;
 CREATE TRIGGER check_ft_150
 AFTER INSERT ON ft_leave_schedule
 FOR EACH ROW EXECUTE PROCEDURE ft_150_constraint();
-
--- REMOVED cos caretaker overlap enforcement + FK constraint should already enforce this
--- -- NONOVERLAPPING constraints for schedule
--- CREATE OR REPLACE FUNCTION not_in_pt_schedule()
--- RETURNS TRIGGER AS 
--- $t$ 
--- DECLARE overlap NUMERIC;
--- BEGIN 
--- 	SELECT COUNT(*) INTO overlap FROM pt_free_schedule WHERE email=NEW.email;
--- 	IF overlap > 0 THEN
--- 		RAISE EXCEPTION 'User is already a part timer!';
--- 	ELSE
--- 		RETURN NEW;
--- 	END IF;
--- END;
--- $t$ LANGUAGE PLpgSQL;
--- CREATE TRIGGER check_pt_schedule
--- BEFORE INSERT ON ft_leave_schedule
--- FOR EACH ROW EXECUTE PROCEDURE not_in_pt_schedule();
-
--- CREATE OR REPLACE FUNCTION not_in_ft_schedule()
--- RETURNS TRIGGER AS 
--- $t$ 
--- DECLARE overlap NUMERIC;
--- BEGIN 
--- 	SELECT COUNT(*) INTO overlap FROM ft_leave_schedule WHERE email=NEW.email;
--- 	IF overlap > 0 THEN
--- 		RAISE EXCEPTION 'User is already a full timer!';
--- 	ELSE
--- 		RETURN NEW;
--- 	END IF;
--- END;
--- $t$ LANGUAGE PLpgSQL;
--- CREATE TRIGGER check_ft_schedule
--- BEFORE INSERT ON pt_free_schedule
--- FOR EACH ROW EXECUTE PROCEDURE not_in_ft_schedule();
-
--- old enumerating of dates
--- select count(*) into transgression FROM 
-	-- 	(select
-	-- 		ac.date as date
-	-- 		from (
-	-- 			select CURRENT_DATE - (a.a + (10 * b.a) + (100 * c.a) + (1000 * d.a) || ' days')::interval as date
-	-- 			from (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as a
-	-- 			cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as b
-	-- 			cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as c
-	-- 			cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as d
-	-- 		) as ac, (select * FROM bid WHERE ct_email=NEW.ct_email) as p
-	-- 		where ac.Date >= p.start_date and ac.Date <= p.end_date 
-	-- 	ORDER BY ac.date) as overlapDates
-	-- group by overlapDates.date
-	-- having count(*) > pet_count;
-
--- CREATE OR REPLACE FUNCTION ft_150_constraint()
--- RETURNS TRIGGER AS 
--- $t$ 
--- DECLARE 
--- 	stretch_end NUMERIC;
--- 	stretch_end2 NUMERIC;
--- 	stretch_start NUMERIC;
--- 	stretch_start2 NUMERIC;
--- 	start_y DOUBLE PRECISION;
--- 	end_y DOUBLE PRECISION;
-
--- BEGIN
--- 	SELECT extract(year from NEW.end_date) into end_y;
--- 	SELECT extract(year from NEW.start_date) into start_y;
-
--- 	SELECT COUNT(*) INTO stretch_start FROM ft_leave_schedule s1
--- 		WHERE extract(year from (s1.start_date - '150 day'::interval)) = extract(year from NEW.start_date)
--- 		AND extract(year FROM s1.start_date) = (start_y)
--- 		AND NOT EXISTS (
--- 			SELECT 1 FROM ft_leave_schedule s2
--- 				WHERE extract(year FROM s2.start_date) = (start_y)
--- 				AND (s1.start_date - '150 day'::interval) <= s2.end_date
--- 				AND s2.start_date < s1.start_date
--- 		);
-
--- 	SELECT COUNT(*) INTO stretch_start2 FROM ft_leave_schedule s1
--- 	WHERE extract(year from (s1.start_date - '300 day'::interval)) = extract(year from NEW.start_date)
--- 	AND extract(year FROM s1.start_date) = (start_y)
--- 	AND NOT EXISTS (
--- 		SELECT 1 FROM ft_leave_schedule s2
--- 		WHERE extract(year FROM s2.start_date) = (start_y)
--- 		AND (s1.start_date - '300 day'::interval) <= s2.end_date
--- 		AND s2.start_date < s1.start_date
--- 	);
-
-	
--- 	SELECT COUNT(*) INTO stretch_end FROM ft_leave_schedule s1
--- 	WHERE extract(year from (s1.end_date + '150 day'::interval)) = extract(year from NEW.end_date)
--- 	AND extract(year FROM s1.end_date) = (end_y)
--- 	AND NOT EXISTS (
--- 		SELECT 1 FROM ft_leave_schedule s2
--- 		WHERE  (s1.end_date + '150 day'::interval) >= s2.start_date
--- 		AND extract(year FROM s2.end_date) = (end_y)
--- 		AND s2.end_date > s1.end_date
--- 	);
-
--- 	SELECT COUNT(*) INTO stretch_end2 FROM ft_leave_schedule s1
--- 	WHERE  extract(year from (s1.end_date + '300 day'::interval)) = extract(year from NEW.end_date)
--- 	AND extract(year FROM s1.end_date) = (end_y)
--- 	AND NOT EXISTS (
--- 		SELECT 1 FROM ft_leave_schedule s2
--- 		WHERE (s1.end_date + '300 day'::interval) >= s2.start_date
--- 		AND extract(year FROM s2.end_date) = (end_y)
--- 		AND s2.end_date > s1.end_date
--- 	);
-
--- 	IF start_y = end_y THEN
--- 		IF stretch_start + stretch_start2 + stretch_end + stretch_end2 < 2 THEN
--- 			RAISE EXCEPTION 'Not enough consecutive working days! %', stretch_start + stretch_start2 + stretch_end + stretch_end2;
--- 		END IF;
--- 	ELSE
--- 		IF stretch_start + stretch_start2 < 2 THEN
--- 			RAISE EXCEPTION 'Not enough consecutive working days! %', stretch_start + stretch_start2;
--- 		ELSIF stretch_end + stretch_end2 < 2 THEN
--- 			RAISE EXCEPTION 'Not enough consecutive working days! %', stretch_end + stretch_end2;
--- 		END IF;
--- 	END IF;
--- 	RETURN NEW;
--- END;
--- $t$ LANGUAGE PLpgSQL;
--- CREATE TRIGGER check_ft_150
--- AFTER INSERT ON ft_leave_schedule
--- FOR EACH ROW EXECUTE PROCEDURE ft_150_constraint();
