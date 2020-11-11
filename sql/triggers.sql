34ac47f71a9c7df1c9e8721d03718f3a67d5fb1a
xiaoyu-nus <tkgs.liu.xiaoyu@gmail.com>
2020-11-11 16:51:48 +0800

    Merge from master

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -39,53 +39,8 @@ CREATE TRIGGER check_pt_ct
 BEFORE INSERT ON part_time_ct
 FOR EACH ROW EXECUTE PROCEDURE not_full_time();
 
-
--- CREATE OR REPLACE FUNCTION ft_rating()
--- RETURNS TRIGGER AS 
--- $t$
--- DECLARE avg_rating NUMERIC;
--- BEGIN
--- 	IF NEW.rating is not NULL THEN
--- 		UPDATE full_time_ct 
--- 		SET (rating) = (SELECT AVG(rating) FROM bid WHERE ct_email=NEW.ct_email AND rating IS NOT NULL) 
--- 		WHERE email=NEW.ct_email;
--- 	END IF;
--- 	RETURN NEW;
--- END;
--- $t$ LANGUAGE PLpgSQL;
-
--- CREATE TRIGGER get_ft_rating
--- BEFORE INSERT OR UPDATE ON bid
--- FOR EACH ROW EXECUTE PROCEDURE ft_rating();
-
-
-CREATE OR REPLACE FUNCTION avg_rating()
-RETURNS TRIGGER AS 
-$t$
-DECLARE avg_rating NUMERIC;
-DECLARE ct_status INTEGER;
-BEGIN
-	SELECT caretaker_status INTO ct_status FROM caretaker WHERE email=NEW.ct_email;
-	IF NEW.rating is not NULL THEN
-		IF ct_status = 2 THEN 
-			UPDATE full_time_ct 
-				SET (rating) = 
-					(SELECT AVG(rating) FROM bid WHERE ct_email=NEW.ct_email AND rating IS NOT NULL) 
-				WHERE email=NEW.ct_email;
-		ELSE
-			UPDATE part_time_ct 
-				SET (rating) =
-					(SELECT AVG(rating) FROM bid WHERE ct_email=NEW.ct_email AND rating IS NOT NULL) 
-				WHERE email=NEW.ct_email;
-		END IF;
-	END IF;
-	RETURN NEW;
-END;
-$t$ LANGUAGE PLpgSQL;
-
-CREATE TRIGGER get_avg_rating
-AFTER INSERT OR UPDATE ON bid
-FOR EACH ROW EXECUTE PROCEDURE avg_rating();
+-- TODO set constraint for bids for schedule -> can't bid for leave
+-- TODO set constraint for schedule with bids -> can't take leave if bids
 
 -- Check that bid made isn't overlapping for pet
 CREATE OR REPLACE FUNCTION no_bid_overlap()
@@ -136,25 +91,25 @@ BEGIN
 END;
 $t$ LANGUAGE PLpgSQL;
 
--- CREATE TRIGGER close_pt_bid
--- AFTER INSERT OR UPDATE ON bid
--- FOR EACH ROW EXECUTE PROCEDURE close_bid();
+CREATE TRIGGER close_pt_bid
+AFTER INSERT OR UPDATE ON bid
+FOR EACH ROW EXECUTE PROCEDURE close_bid()
 
--- CREATE OR REPLACE FUNCTION prevent_update_bid()
--- RETURNS TRIGGER AS
--- $t$
--- BEGIN
--- 	IF OLD.bid_status = 'closed' THEN
--- 		RAISE EXCEPTION 'The bid is already closed';
--- 	ELSE
--- 		RETURN NEW;
--- 	END IF;
--- END;
--- $t$ LANGUAGE PLpgSQL;
+CREATE OR REPLACE FUNCTION prevent_update_bid()
+RETURNS TRIGGER AS
+$t$
+BEGIN
+	IF OLD.bid_status = 'closed' THEN
+		RAISE EXCEPTION 'The bid is already closed';
+	ELSE
+		RETURN NEW;
+	END IF;
+END;
+$t$ LANGUAGE PLpgSQL;
 
--- CREATE TRIGGER prevent_bid
--- BEFORE UPDATE ON bid
--- FOR EACH ROW EXECUTE PROCEDURE prevent_update_bid();
+CREATE TRIGGER prevent_bid
+BEFORE UPDATE ON bid
+FOR EACH ROW EXECUTE PROCEDURE prevent_update_bid();
 
 
 -- for debugging
@@ -264,145 +219,77 @@ FOR EACH ROW EXECUTE PROCEDURE date_non_overlap_ft_schedule();
 -- CREATE TABLE count_sched (
 -- 	c1 int
 -- );
+
+
 CREATE OR REPLACE FUNCTION ft_150_constraint()
 RETURNS TRIGGER AS 
 $t$ 
 DECLARE 
-	stretch_end NUMERIC;
-	stretch_end2 NUMERIC;
-	stretch_start NUMERIC;
-	stretch_start2 NUMERIC;
-	start_y DOUBLE PRECISION;
-	end_y DOUBLE PRECISION;
-
+	count_150 NUMERIC;
+	count_300 NUMERIC;
+	new_end_year NUMERIC;
+	new_start_year NUMERIC;
 BEGIN
-	SELECT extract(year from NEW.end_date) into end_y;
-	SELECT extract(year from NEW.start_date) into start_y;
 
-	-- somehow can't create view with NEW.end_date inside the WHERE clause, 
-	-- so we expanded the view into the queries used for checking constraint
-	
-	-- CREATE VIEW start_year (email, start_date, end_date) AS (
-	-- 	SELECT * FROM ft_leave_schedule ft
-	-- 	WHERE (SELECT extract(year from new.end_date)) = (select extract(year FROM ft.start_date))
-	-- );
+	SELECT extract(year from NEW.start_date) into new_start_year;
+	SELECT extract(year from NEW.end_date) into new_end_year;
 
-	-- CREATE VIEW end_year (email, start_date, end_date) AS (
-	-- 	SELECT * FROM ft_leave_schedule ft
-	-- 	WHERE (select extract(year FROM ft.end_date)) = (select extract(year FROM NEW.end_date))
-	-- );
+	select COUNT(*) into count_150 FROM ( 
+		select * from (
+			select *, row_number() over (partition by 1) as r1 from (
+				select (Date(new_start_year||'-01-01')-'1 day'::interval) as ed1
+				union
+				SELECT end_date as ed1 FROM ft_leave_schedule f1
+				WHERE email=NEW.email AND start_date >= Date(new_start_year||'-01-01') order by ed1 ASC
+			) t1 
+		) ord1 inner join 
+		(
+			select *, row_number() over (partition by 1) as r2 from (
+				select (Date(new_end_year||'-01-01')+'1 year'::interval) as sd2
+				union
+				select start_date as sd2 FROM ft_leave_schedule f2
+				WHERE email=NEW.email AND start_date >= Date(new_start_year||'-01-01') order by sd2 ASC
+			) t2
+		) ord2 on ord1.r1=ord2.r2
+	) as cc
+	WHERE Date(cc.sd2)-Date(cc.ed1) > 150;
 
-	SELECT COUNT(*) INTO stretch_start FROM ft_leave_schedule s1
-		WHERE extract(year from (s1.start_date - '150 day'::interval)) = extract(year from NEW.start_date)
-		AND extract(year FROM s1.start_date) = (start_y)
-		AND NOT EXISTS (
-			SELECT 1 FROM ft_leave_schedule s2
-				WHERE extract(year FROM s2.start_date) = (start_y)
-				AND (s1.start_date - '150 day'::interval) <= s2.end_date
-				AND s2.start_date < s1.start_date
-		);
+	select COUNT(*) into count_300 FROM ( 
+		select * from (
+			select *, row_number() over (partition by 1) as r1 from (
+				select (Date(new_start_year||'-01-01')-'1 day'::interval) as ed1
+				union
+				SELECT end_date as ed1 FROM ft_leave_schedule f1
+				WHERE email=NEW.email AND start_date >= Date(new_start_year||'-01-01') order by ed1 ASC
+			) t1 
+		) ord1 inner join 
+		(
+			select *, row_number() over (partition by 1) as r2 from (
+				select (Date(new_end_year||'-01-01')+'1 year'::interval) as sd2
+				union
+				select start_date as sd2 FROM ft_leave_schedule f2
+				WHERE email=NEW.email AND start_date >= Date(new_start_year||'-01-01') order by sd2 ASC
+			) t2
+		) ord2 on ord1.r1=ord2.r2
+	) as cc
+	WHERE Date(cc.sd2)-Date(cc.ed1) > 300;
 
-	SELECT COUNT(*) INTO stretch_start2 FROM ft_leave_schedule s1
-	WHERE extract(year from (s1.start_date - '300 day'::interval)) = extract(year from NEW.start_date)
-	AND extract(year FROM s1.start_date) = (start_y)
-	AND NOT EXISTS (
-		SELECT 1 FROM ft_leave_schedule s2
-		WHERE extract(year FROM s2.start_date) = (start_y)
-		AND (s1.start_date - '300 day'::interval) <= s2.end_date
-		AND s2.start_date < s1.start_date
-	);
-
-	
-	SELECT COUNT(*) INTO stretch_end FROM ft_leave_schedule s1
-	WHERE extract(year from (s1.end_date + '150 day'::interval)) = extract(year from NEW.end_date)
-	AND extract(year FROM s1.end_date) = (end_y)
-	AND NOT EXISTS (
-		SELECT 1 FROM ft_leave_schedule s2
-		WHERE  (s1.end_date + '150 day'::interval) >= s2.start_date
-		AND extract(year FROM s2.end_date) = (end_y)
-		AND s2.end_date > s1.end_date
-	);
-
-	SELECT COUNT(*) INTO stretch_end2 FROM ft_leave_schedule s1
-	WHERE  extract(year from (s1.end_date + '300 day'::interval)) = extract(year from NEW.end_date)
-	AND extract(year FROM s1.end_date) = (end_y)
-	AND NOT EXISTS (
-		SELECT 1 FROM ft_leave_schedule s2
-		WHERE (s1.end_date + '300 day'::interval) >= s2.start_date
-		AND extract(year FROM s2.end_date) = (end_y)
-		AND s2.end_date > s1.end_date
-	);
-
-	-- INSERT INTO count_sched VALUES (stretch_start + stretch_start2 + stretch_end + stretch_end2);
-
-	IF start_y = end_y THEN
-		IF stretch_start + stretch_start2 + stretch_end + stretch_end2 < 2 THEN
-			RAISE EXCEPTION 'Not enough consecutive working days! %', stretch_start + stretch_start2 + stretch_end + stretch_end2;
+	IF new_start_year != new_end_year THEN
+		IF count_300 = 2 OR count_150 = 4 OR (count_150 = 2 AND count_300 = 1) THEN 
+			RETURN NEW;
+		ELSE
+			RAISE EXCEPTION 'No 2 times 150 consecutive working days!';
 		END IF;
 	ELSE
-		IF stretch_start + stretch_start2 < 2 THEN
-			RAISE EXCEPTION 'Not enough consecutive working days! %', stretch_start + stretch_start2;
-		ELSIF stretch_end + stretch_end2 < 2 THEN
-			RAISE EXCEPTION 'Not enough consecutive working days! %', stretch_end + stretch_end2;
+		IF count_150 = 2 OR count_300 = 1 THEN 
+			RETURN NEW;
+		ELSE
+			RAISE EXCEPTION 'No 2 times 150 consecutive working days!';
 		END IF;
 	END IF;
-	RETURN NEW;
+	
 END;
 $t$ LANGUAGE PLpgSQL;
 CREATE TRIGGER check_ft_150
 AFTER INSERT ON ft_leave_schedule
 FOR EACH ROW EXECUTE PROCEDURE ft_150_constraint();
-
-
-
--- REMOVED cos caretaker overlap enforcement + FK constraint should already enforce this
--- -- NONOVERLAPPING constraints for schedule
--- CREATE OR REPLACE FUNCTION not_in_pt_schedule()
--- RETURNS TRIGGER AS 
--- $t$ 
--- DECLARE overlap NUMERIC;
--- BEGIN 
--- 	SELECT COUNT(*) INTO overlap FROM pt_free_schedule WHERE email=NEW.email;
--- 	IF overlap > 0 THEN
--- 		RAISE EXCEPTION 'User is already a part timer!';
--- 	ELSE
--- 		RETURN NEW;
--- 	END IF;
--- END;
--- $t$ LANGUAGE PLpgSQL;
--- CREATE TRIGGER check_pt_schedule
--- BEFORE INSERT ON ft_leave_schedule
--- FOR EACH ROW EXECUTE PROCEDURE not_in_pt_schedule();
-
--- CREATE OR REPLACE FUNCTION not_in_ft_schedule()
--- RETURNS TRIGGER AS 
--- $t$ 
--- DECLARE overlap NUMERIC;
--- BEGIN 
--- 	SELECT COUNT(*) INTO overlap FROM ft_leave_schedule WHERE email=NEW.email;
--- 	IF overlap > 0 THEN
--- 		RAISE EXCEPTION 'User is already a full timer!';
--- 	ELSE
--- 		RETURN NEW;
--- 	END IF;
--- END;
--- $t$ LANGUAGE PLpgSQL;
--- CREATE TRIGGER check_ft_schedule
--- BEFORE INSERT ON pt_free_schedule
--- FOR EACH ROW EXECUTE PROCEDURE not_in_ft_schedule();
-
--- old enumerating of dates
--- select count(*) into transgression FROM 
-	-- 	(select
-	-- 		ac.date as date
-	-- 		from (
-	-- 			select CURRENT_DATE - (a.a + (10 * b.a) + (100 * c.a) + (1000 * d.a) || ' days')::interval as date
-	-- 			from (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as a
-	-- 			cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as b
-	-- 			cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as c
-	-- 			cross join (select 0 as a union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) as d
-	-- 		) as ac, (select * FROM bid WHERE ct_email=NEW.ct_email) as p
-	-- 		where ac.Date >= p.start_date and ac.Date <= p.end_date 
-	-- 	ORDER BY ac.date) as overlapDates
-	-- group by overlapDates.date
-	-- having count(*) > pet_count;
