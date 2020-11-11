919ab411957ac93fbf8bb390837928b6390dc8f7
xiaoyu-nus <tkgs.liu.xiaoyu@gmail.com>
2020-11-11 16:58:11 +0800

    Merge from master

--- sql/schema.sql
+++ sql/schema.sql
@@ -11,10 +11,6 @@ DROP VIEW IF EXISTS caretaker CASCADE;
 DROP TABLE IF EXISTS full_time_ct CASCADE;
 DROP TABLE IF EXISTS part_time_ct CASCADE;
 DROP TABLE IF EXISTS person CASCADE;
-<<<<<<< HEAD
-
-=======
->>>>>>> f1af6f6ecdc365bb2a882dc2ac9c98d2b4d7ef6a
 DROP TABLE IF EXISTS bid;
 DROP TYPE IF EXISTS user_role;
 DROP TYPE IF EXISTS transport_method;
@@ -69,15 +65,9 @@ CREATE TABLE full_time_ct (
 );
 
 CREATE VIEW caretaker (email, caretaker_status, rating) AS (
-<<<<<<< HEAD
 	SELECT email, 1, rating FROM  part_time_ct 
 	UNION 
 	SELECT email, 2, rating FROM full_time_ct
-=======
-	SELECT email, 1, 4.1 FROM  part_time_ct 
-	UNION 
-	SELECT email, 2, 4.2 FROM full_time_ct
->>>>>>> f1af6f6ecdc365bb2a882dc2ac9c98d2b4d7ef6a
 );
 
 CREATE TABLE pt_specializes_in (
@@ -120,10 +110,6 @@ CREATE TABLE ft_leave_schedule (
 
 
 CREATE TABLE bid (
-<<<<<<< HEAD
-
-=======
->>>>>>> f1af6f6ecdc365bb2a882dc2ac9c98d2b4d7ef6a
 	ct_email varchar(64) REFERENCES person(email),
 	ct_price int NOT NULL,
 	start_date DATE NOT NULL,
@@ -133,15 +119,9 @@ CREATE TABLE bid (
 	transport_method transport_method NOT NULL,
 	pet_owner varchar(64),
 	pet_name varchar(64),
-<<<<<<< HEAD
 	bid_status bid_status NOT NULL,
 	feedback text DEFAULT NULL,
 	rating int DEFAULT NULL,
-=======
-	pet_category varchar(64) REFERENCES pet_category(type_name),
-	bid_status bid_status NOT NULL,
-	feedback text,
->>>>>>> f1af6f6ecdc365bb2a882dc2ac9c98d2b4d7ef6a
 	FOREIGN KEY (pet_owner, credit_card) REFERENCES credit_card(cardholder, card_number),
 	FOREIGN KEY (pet_owner, pet_name) REFERENCES pet(owner, name),
 	CONSTRAINT bid_id PRIMARY KEY (ct_email, pet_name, pet_owner, start_date, end_date),
