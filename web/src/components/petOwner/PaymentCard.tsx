eda37a93ff40aeff8642674b60425255e63a8180
Tianyi <42670338+tysng@users.noreply.github.com>
2020-11-13 12:22:57 +0800

    Fix credit card information formating

--- web/src/components/petOwner/PaymentCard.tsx
+++ web/src/components/petOwner/PaymentCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Card, Descriptions, Modal } from "antd";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import { CreditCard } from "../../../../models/creditCard";
-import Meta from "antd/lib/card/Meta";
+import moment from "moment";
 
 type CreditCardProps = {
     creditCard: CreditCard;
@@ -10,8 +10,20 @@ type CreditCardProps = {
     onDelete: (rec: Omit<CreditCard, "cardholder">) => void;
 };
 
+const formatCreditCardNumber = (num: number) => {
+    const str = num.toString();
+    const chunks: string[] = new Array(4);
+
+    for (let i = 0, o = 0; i < 4; ++i, o += 4) {
+        chunks[i] = str.substr(o, 4);
+    }
+
+    return chunks.reduce((a, b) => a + " " + b);
+};
+
 const PaymentCard = (props: CreditCardProps) => {
     const { creditCard, generateModal, onDelete } = props;
+
     return (
         <Card
             actions={[
@@ -25,7 +37,8 @@ const PaymentCard = (props: CreditCardProps) => {
                     key="delete"
                     onClick={() => {
                         Modal.confirm({
-                            title: "Do you really want to delete this credit card?",
+                            title:
+                                "Do you really want to delete this credit card?",
                             onOk: () => {
                                 onDelete(creditCard);
                             },
@@ -34,13 +47,20 @@ const PaymentCard = (props: CreditCardProps) => {
                     }}
                 />
             ]}
-            style={{ height: "100%", width: 500}}
+            style={{ height: "100%" }}
         >
-                    <>
-                        <p>{"Card Number: "} {creditCard.cardNumber} </p>
-                        <p> {"Expiry Date: "} {creditCard.expiryDate} </p>
-                        <p> {"Security Code: "} {creditCard.securityCode} </p>
-                    </>
+            <>
+                <p>
+                    {"Card Number: "}{" "}
+                    {formatCreditCardNumber(creditCard.cardNumber)}{" "}
+                </p>
+                <p>
+                    {" "}
+                    {"Expiry Date: " +
+                        moment(creditCard.expiryDate).format("MM/YYYY")}
+                </p>
+                <p> {"Security Code: ***"}</p>
+            </>
         </Card>
     );
 };
